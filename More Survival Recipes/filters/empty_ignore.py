7f5d8c4e8d0106385467a4bd261dde4891331fe1
DrinkWater623 <DrinkWater623@outlook.com>
2023-09-03 11:27:29 -0700

    Adding New Add-on More Survival Recipes

---
+++ More Survival Recipes/filters/empty_ignore.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import os
+#import shutil
+
+def delete_empty_folders(current_dir: Path):    
+    
+    for folder_path in current_dir.iterdir():        
+
+        if folder_path.is_dir():
+            
+            if len(os.listdir(folder_path)) >= 1:
+                delete_empty_folders(folder_path)
+
+            #Not Else, because when done above, the folder may be empty
+            if len(os.listdir(folder_path)) == 0: 
+                try:                    
+                    os.rmdir(folder_path)
+                    print(f"Deleted empty folder: {folder_path}")
+                except Exception as e:
+                    print(f"Error while deleting empty folder {folder_path}: {e}")
+
+if __name__ == "__main__":
+    delete_empty_folders(Path('./BP'))
+    delete_empty_folders(Path('./RP'))
